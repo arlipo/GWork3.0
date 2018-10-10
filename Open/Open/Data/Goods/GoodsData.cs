9bb86f5aadaa6f1cc2d70f83c1f5ede315c418a1
Artjom Lipodat <arlipo@ttu.ee>
2018-10-10 15:00:43 +0300

    goods

---
+++ Open/Open/Data/Goods/GoodsData.cs
@@ -0,0 +1,36 @@
+﻿using Open.Data.Common;
+
+namespace Open.Data.Goods
+{
+    public class GoodsData : NamedData
+    {
+        private string imageType;
+        private string description;
+        private string fileLocation;
+        private string price;
+
+        public string ImageType
+        {
+            get => getString(ref imageType);
+            set => imageType = value;
+        }
+
+        public string Description
+        {
+            get => getString(ref description);
+            set => description = value;
+        }
+
+        public string FileLocation
+        {
+            get => getString(ref fileLocation);
+            set => fileLocation = value;
+        }
+
+        public string Price
+        {
+            get => getString(ref price);
+            set => price = value;
+        }
+    }
+}
---
+++ Open/Open/Domain/Goods/Good.cs
@@ -0,0 +1,10 @@
+﻿using Open.Data.Goods;
+using Open.Domain.Common;
+
+namespace Open.Domain.Goods
+{
+    public class Good : NamedEntity<GoodsData>
+    {
+        public Good(GoodsData r) : base(r) { }
+    }
+}
