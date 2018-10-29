4cd01e97dd91ffb0a093c91a851429ebd812d2d4
Yana Sirotkina <yasiro@ttu.ee>
2018-10-29 12:08:06 +0200

    ssqlka na glavnuju kartinku ispravlena, poprobovala dobavitj tovar v goods

--- Open/Open/Infra/Goods/GoodsInitializer.cs
+++ Open/Open/Infra/Goods/GoodsInitializer.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Open.Data.Goods;
 
 namespace Open.Infra.Goods
 {
@@ -10,6 +13,30 @@ namespace Open.Infra.Goods
             if (c.Goods.Any()) return;
             //здесь заполняется база данных с товарами
             c.SaveChanges();
+        }
+
+        private static List<string> goodsList(SentryDbContext c)
+        {
+            var l = new List<string> {
+                add(c, new GoodsData{
+                    Name = "Antifreeze + coolant LONG LIFE",
+                    Code = "12345",
+                    Description = "Organic Acid Technology (OAT) coolant technology that is compatible" +
+                                  " for use in all automobiles and light-duty trucks, regardless of make, " +
+                                  "model, year or original antifreeze color.",
+                    FileLocation = "hz",
+                    ID = "12345",
+                    ImageType = "png",
+                    Price = "8,99"
+                })
+            };
+            return l;
+        }
+        private static string add(SentryDbContext c, GoodsData goods)
+        {
+            goods.ID = Guid.NewGuid().ToString();
+            c.Goods.Add(goods);
+            return goods.ID;
         }
     }
 }
