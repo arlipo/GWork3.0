b93e1a031badfcd3ac97de9ad4eb9d7d0c5eaab2
Luizi-Anna Meshalkina <lumesh@ttu.ee>
2018-11-01 17:47:00 +0200

    controller

--- Open/Open/Sentry/Controllers/GoodsController.cs
+++ Open/Open/Sentry/Controllers/GoodsController.cs
@@ -1,6 +1,10 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Open.Aids;
+using Open.Core;
 using Open.Domain.Goods;
 using Open.Facade.Goods;
 
@@ -24,6 +28,56 @@ namespace Open.Sentry.Controllers
             return View(new GoodViewsList(l));
         }
 
+
+
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            var c = await repository.GetObject(id);
+            return View(GoodViewFactory.Create(c));
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit([Bind(properties)] GoodView c)
+        {
+            if (!ModelState.IsValid) return View(c);
+            var o = await repository.GetObject(c.Code);
+            o.Data.Name = c.Name;
+            o.Data.Code = c.Description;
+            o.Data.Code = c.FileLocation;
+            o.Data.Code = c.Price;
+
+
+            await repository.UpdateObject(o);
+            return RedirectToAction("Index");
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind(properties)] GoodView c)
+        {
+            await validateId(c.Code, ModelState);
+            if (!ModelState.IsValid) return View(c);
+            //var o = GoodFactory.Create(c.Code, c.Description, c.FileLocation,c.ImageType, c.Name, c.Price);
+            //await repository.AddObject(o);
+            return RedirectToAction("Index");
+        }
+
+        private async Task validateId(string id, ModelStateDictionary d)
+        {
+            if (await isIdInUse(id))
+                d.AddModelError(string.Empty, idIsInUseMessage(id));
+        }
+        private async Task<bool> isIdInUse(string id)
+        {
+            return (await repository.GetObject(id))?.Data?.ID == id;
+        }
+        private static string idIsInUseMessage(string id)
+        {
+            var name = GetMember.DisplayName<GoodView>(c => c.Code);
+            return string.Format(Messages.ValueIsAlreadyInUse, id, name);
+        }
         // GET: Goods/Create
         /*
         public IActionResult Create()
