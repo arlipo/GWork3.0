a06af8c800c1ea8fd0a097697529136b3857c728
Luizi-Anna Meshalkina <lumesh@ttu.ee>
2018-11-08 20:01:15 +0200

    posmotrite startup (comment)

--- Open/Open/Sentry/Models/AccountViewModels/LoginViewModel.cs
+++ Open/Open/Sentry/Models/AccountViewModels/LoginViewModel.cs
@@ -5,10 +5,12 @@ namespace Open.Sentry.Models.AccountViewModels
     {
         [Required]
         [EmailAddress]
+        [Display(Name = "Email Address")]
         public string Email { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string Password { get; set; }
 
         [Display(Name = "Remember me?")]
--- Open/Open/Sentry/Startup.cs
+++ Open/Open/Sentry/Startup.cs
@@ -17,6 +17,8 @@ using Open.Infra.Quantity;
 using Open.Sentry.Data;
 using Open.Sentry.Models;
 using Open.Sentry.Services;
+using System.Threading.Tasks;
+
 namespace Open.Sentry {
     public class Startup {
         public Startup(IConfiguration configuration) {
@@ -74,6 +76,51 @@ namespace Open.Sentry {
             }
             else { app.UseExceptionHandler("/Home/Error"); }
             }
-        }
+
+    }
+
+
+    //private async Task TaskAsync(ServiceProvider serviceProvider)
+    //{
+    //    //initializing custom roles 
+    //    var RoleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    //    var UserManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+    //    string[] roleNames = { "Admin", "Store-Manager", "Member" };
+    //    IdentityResult roleResult;
+
+    //    foreach (var roleName in roleNames)
+    //    {
+    //        var roleExist = await RoleManager.RoleExistsAsync(roleName);
+    //        // ensure that the role does not exist
+    //        if (!roleExist)
+    //        {
+    //            //create the roles and seed them to the database: 
+    //            roleResult = await RoleManager.CreateAsync(new IdentityRole(roleName));
+    //        }
+    //    }
+
+    //    // find the user with the admin email 
+    //    var _user = await UserManager.FindByEmailAsync("admin@email.com");
+
+    //    // check if the user exists
+    //    if (_user == null)
+    //    {
+    //        //Here you could create the super admin who will maintain the web app
+    //        var poweruser = new ApplicationUser
+    //        {
+    //            UserName = "Admin",
+    //            Email = "admin@email.com",
+    //        };
+    //        string adminPassword = "12345";
+
+    //        var createPowerUser = await UserManager.CreateAsync(poweruser, adminPassword);
+    //        if (createPowerUser.Succeeded)
+    //        {
+    //            //here we tie the new user to the role
+    //            await UserManager.AddToRoleAsync(poweruser, "Admin");
+
+    //        }
+    //    }
+    //}
 }
 
