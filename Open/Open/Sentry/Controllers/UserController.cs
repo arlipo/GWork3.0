d4f1acdcbc9adaf710609977c30ad78af24e9678
Yana Sirotkina <yasiro@ttu.ee>
2018-11-09 15:38:40 +0200

    CustomersInitializer, User

--- Open/Open/Sentry/Controllers/UserController.cs
+++ Open/Open/Sentry/Controllers/UserController.cs
@@ -1,21 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Open.Domain.Users;
 
 
-namespace Open.Sentry.Controllers { 
-[Authorize]
-public class UsersController : Controller
+namespace Open.Sentry.Controllers
 {
-    private readonly IUsersRepository repository;
-    internal const string properties = "ID, Name, Code, Phone, Address";
-    public UsersController(IUsersRepository r)
+    public class UserController : Controller
     {
-        repository = r;
+        [Authorize]
+        public class UsersController : Controller
+        {
+            private readonly IUsersRepository repository;
+            internal const string properties = "ID, Name, Code, Phone, Address";
+
+            public UsersController(IUsersRepository r)
+            {
+                repository = r;
+            }
+        }
     }
-}
 }
