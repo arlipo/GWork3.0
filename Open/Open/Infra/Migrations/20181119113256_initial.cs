b6c4b4df1422f12ddb3b1b6bbdbdf75b16d3247f
Artjom Lipodat <arlipo@ttu.ee>
2018-11-19 13:36:40 +0200

    mg

---
+++ Open/Open/Infra/Migrations/20181119113256_initial.cs
@@ -0,0 +1,376 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Open.Infra.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Country",
+                columns: table => new
+                {
+                    ValidFrom = table.Column<DateTime>(nullable: false),
+                    ValidTo = table.Column<DateTime>(nullable: false),
+                    Code = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    ID = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Country", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Currency",
+                columns: table => new
+                {
+                    ValidFrom = table.Column<DateTime>(nullable: false),
+                    ValidTo = table.Column<DateTime>(nullable: false),
+                    Code = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    ID = table.Column<string>(nullable: false),
+                    Definition = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Currency", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Good",
+                columns: table => new
+                {
+                    ValidFrom = table.Column<DateTime>(nullable: false),
+                    ValidTo = table.Column<DateTime>(nullable: false),
+                    Code = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    ID = table.Column<string>(nullable: false),
+                    ImageType = table.Column<string>(nullable: true),
+                    Image = table.Column<byte[]>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    PicFileLocation = table.Column<string>(nullable: true),
+                    Price = table.Column<string>(nullable: true),
+                    Type = table.Column<int>(nullable: false),
+                    Discriminator = table.Column<string>(nullable: false),
+                    Volume = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Good", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RateType",
+                columns: table => new
+                {
+                    ValidFrom = table.Column<DateTime>(nullable: false),
+                    ValidTo = table.Column<DateTime>(nullable: false),
+                    Code = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    ID = table.Column<string>(nullable: false),
+                    Description = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RateType", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    ValidFrom = table.Column<DateTime>(nullable: false),
+                    ValidTo = table.Column<DateTime>(nullable: false),
+                    Code = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    ID = table.Column<string>(nullable: false),
+                    Phone = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    Login = table.Column<string>(nullable: true),
+                    Password = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Address",
+                columns: table => new
+                {
+                    ValidFrom = table.Column<DateTime>(nullable: false),
+                    ValidTo = table.Column<DateTime>(nullable: false),
+                    ID = table.Column<string>(nullable: false),
+                    Address = table.Column<string>(nullable: true),
+                    CityOrAreaCode = table.Column<string>(nullable: true),
+                    RegionOrStateOrCountryCode = table.Column<string>(nullable: true),
+                    ZipOrPostCodeOrExtension = table.Column<string>(nullable: true),
+                    Discriminator = table.Column<string>(nullable: false),
+                    CountryID = table.Column<string>(nullable: true),
+                    NationalDirectDialingPrefix = table.Column<string>(nullable: true),
+                    Device = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Address", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Address_Country_CountryID",
+                        column: x => x.CountryID,
+                        principalTable: "Country",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NationalCurrency",
+                columns: table => new
+                {
+                    ValidFrom = table.Column<DateTime>(nullable: false),
+                    ValidTo = table.Column<DateTime>(nullable: false),
+                    CountryID = table.Column<string>(nullable: false),
+                    CurrencyID = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NationalCurrency", x => new { x.CountryID, x.CurrencyID });
+                    table.ForeignKey(
+                        name: "FK_NationalCurrency_Country_CountryID",
+                        column: x => x.CountryID,
+                        principalTable: "Country",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_NationalCurrency_Currency_CurrencyID",
+                        column: x => x.CurrencyID,
+                        principalTable: "Currency",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PaymentMethod",
+                columns: table => new
+                {
+                    ValidFrom = table.Column<DateTime>(nullable: false),
+                    ValidTo = table.Column<DateTime>(nullable: false),
+                    ID = table.Column<string>(nullable: false),
+                    DailyLimit = table.Column<decimal>(nullable: false),
+                    Code = table.Column<string>(nullable: true),
+                    Issue = table.Column<string>(nullable: true),
+                    Address = table.Column<string>(nullable: true),
+                    Number = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    Organization = table.Column<string>(nullable: true),
+                    CurrencyID = table.Column<string>(nullable: true),
+                    Discriminator = table.Column<string>(nullable: false),
+                    Payee = table.Column<string>(nullable: true),
+                    CreditLimit = table.Column<decimal>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentMethod", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_PaymentMethod_Currency_CurrencyID",
+                        column: x => x.CurrencyID,
+                        principalTable: "Currency",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ShoppingCartItems",
+                columns: table => new
+                {
+                    ValidFrom = table.Column<DateTime>(nullable: false),
+                    ValidTo = table.Column<DateTime>(nullable: false),
+                    Code = table.Column<string>(nullable: true),
+                    Name = table.Column<string>(nullable: true),
+                    ID = table.Column<string>(nullable: false),
+                    ShoppingCartItemId = table.Column<int>(nullable: false),
+                    GoodsID = table.Column<string>(nullable: true),
+                    Amount = table.Column<int>(nullable: false),
+                    ShoppingCartId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ShoppingCartItems", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_ShoppingCartItems_Good_GoodsID",
+                        column: x => x.GoodsID,
+                        principalTable: "Good",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rate",
+                columns: table => new
+                {
+                    ValidFrom = table.Column<DateTime>(nullable: false),
+                    ValidTo = table.Column<DateTime>(nullable: false),
+                    ID = table.Column<string>(nullable: false),
+                    CurrencyID = table.Column<string>(nullable: true),
+                    RateTypeID = table.Column<string>(nullable: true),
+                    Rate = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rate", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Rate_Currency_CurrencyID",
+                        column: x => x.CurrencyID,
+                        principalTable: "Currency",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Rate_RateType_RateTypeID",
+                        column: x => x.RateTypeID,
+                        principalTable: "RateType",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TelecomDeviceRegistration",
+                columns: table => new
+                {
+                    ValidFrom = table.Column<DateTime>(nullable: false),
+                    ValidTo = table.Column<DateTime>(nullable: false),
+                    AddressID = table.Column<string>(nullable: false),
+                    DeviceID = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TelecomDeviceRegistration", x => new { x.AddressID, x.DeviceID });
+                    table.ForeignKey(
+                        name: "FK_TelecomDeviceRegistration_Address_AddressID",
+                        column: x => x.AddressID,
+                        principalTable: "Address",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_TelecomDeviceRegistration_Address_DeviceID",
+                        column: x => x.DeviceID,
+                        principalTable: "Address",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Payment",
+                columns: table => new
+                {
+                    ValidFrom = table.Column<DateTime>(nullable: false),
+                    ValidTo = table.Column<DateTime>(nullable: false),
+                    ID = table.Column<string>(nullable: false),
+                    Amount = table.Column<decimal>(nullable: false),
+                    DateDue = table.Column<DateTime>(nullable: false),
+                    DateMade = table.Column<DateTime>(nullable: false),
+                    CurrencyID = table.Column<string>(nullable: true),
+                    PaymentMethodID = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Payment", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Payment_Currency_CurrencyID",
+                        column: x => x.CurrencyID,
+                        principalTable: "Currency",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_Payment_PaymentMethod_PaymentMethodID",
+                        column: x => x.PaymentMethodID,
+                        principalTable: "PaymentMethod",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Address_CountryID",
+                table: "Address",
+                column: "CountryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_NationalCurrency_CurrencyID",
+                table: "NationalCurrency",
+                column: "CurrencyID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payment_CurrencyID",
+                table: "Payment",
+                column: "CurrencyID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payment_PaymentMethodID",
+                table: "Payment",
+                column: "PaymentMethodID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PaymentMethod_CurrencyID",
+                table: "PaymentMethod",
+                column: "CurrencyID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rate_CurrencyID",
+                table: "Rate",
+                column: "CurrencyID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rate_RateTypeID",
+                table: "Rate",
+                column: "RateTypeID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ShoppingCartItems_GoodsID",
+                table: "ShoppingCartItems",
+                column: "GoodsID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TelecomDeviceRegistration_DeviceID",
+                table: "TelecomDeviceRegistration",
+                column: "DeviceID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "NationalCurrency");
+
+            migrationBuilder.DropTable(
+                name: "Payment");
+
+            migrationBuilder.DropTable(
+                name: "Rate");
+
+            migrationBuilder.DropTable(
+                name: "ShoppingCartItems");
+
+            migrationBuilder.DropTable(
+                name: "TelecomDeviceRegistration");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "PaymentMethod");
+
+            migrationBuilder.DropTable(
+                name: "RateType");
+
+            migrationBuilder.DropTable(
+                name: "Good");
+
+            migrationBuilder.DropTable(
+                name: "Address");
+
+            migrationBuilder.DropTable(
+                name: "Currency");
+
+            migrationBuilder.DropTable(
+                name: "Country");
+        }
+    }
+}
