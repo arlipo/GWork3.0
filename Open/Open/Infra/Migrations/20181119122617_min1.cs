68d635ded58d91e1c0cd9d8c097fb728f1ffb5ea
Luizi-Anna Meshalkina <lumesh@ttu.ee>
2018-11-20 17:27:28 +0200

    migrachii

--- Open/Open/Infra/Migrations/20181119122617_min1.cs
+++ Open/Open/Infra/Migrations/20181119122617_min1.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Open.Infra.Migrations
 {
-    public partial class initial : Migration
+    public partial class min1 : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
