6a5e2229ead993807a0c62d5a8baf91500742bd8
Artjom Lipodat <arlipo@ttu.ee>
2018-11-26 11:19:26 +0200

    shc

---
+++ Open/Open/Domain/ShoppingCart/CartItem.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Open.Data.Goods;
+
+namespace Open.Domain.ShoppingCart
+{
+    public class CartItem : IEquatable<CartItem>
+    {
+        public int Quantity { get; set; }
+
+        private int _productId;
+
+        public int ProductId
+        {
+            get => _productId;
+            set
+            {
+                _product = null;
+                _productId = value;
+            }
+        }
+
+        private GoodsData _product = null;
+
+        public GoodsData Prod
+        {
+            get
+            {
+                if (_product == null)
+                    _product = new GoodsData();
+                return _product;
+            }
+        }
+
+        public string Description => Prod.Description;
+
+        public decimal UnitPrice => decimal.Parse(Prod.Price);
+
+        public decimal TotalPrice => UnitPrice * Quantity;
+
+        public CartItem(int productId)
+        {
+            ProductId = productId;
+        }
+
+        public bool Equals(CartItem item) => item.ProductId == ProductId;
+    }
+}
---
+++ Open/Open/Domain/ShoppingCart/ShoppingCart.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Web;
+
+namespace Open.Domain.ShoppingCart
+{
+    public class ShoppingCart
+    {
+        public List<CartItem> Items { get; private set; }
+
+        public static readonly ShoppingCart Instance;
+
+        static ShoppingCart()
+        {
+            if (HttpContext.Current.Session["ASPNETShoppingCart"] == null)
+            {
+                Instance = new ShoppingCart();
+                Instance.Items = new List<CartItem>();
+                HttpContext.Current.Session["ASPNETShoppingCart"] = Instance;
+            }
+            else Instance = (ShoppingCart) HttpContext.Current.Session["ASPNETShoppingCart"];
+        }
+
+        protected ShoppingCart() { }
+
+        public void AddItem(int productId)
+        {
+            CartItem newItem = new CartItem(productId);
+        }
+    }
+}
