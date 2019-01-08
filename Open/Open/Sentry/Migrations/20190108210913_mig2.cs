d537b7b8f56a1fff51ecff24690716a38099ad19
Luizi-Anna Meshalkina <lumesh@ttu.ee>
2019-01-09 00:22:14 +0200

    l

--- Open/Open/Sentry/Migrations/20190108210913_mig2.cs
+++ Open/Open/Sentry/Migrations/20190108210913_mig2.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace Open.Sentry.Migrations
 {
-    public partial class initial2 : Migration
+    public partial class mig2 : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
