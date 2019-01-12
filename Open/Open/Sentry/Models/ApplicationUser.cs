0ae21c738c39754cb3dbb85a617a384ae2af3db4
Artjom Lipodat <arlipo@ttu.ee>
2019-01-12 17:41:45 +0200

    remove/add credits logic

--- Open/Open/Sentry/Models/ApplicationUser.cs
+++ Open/Open/Sentry/Models/ApplicationUser.cs
@@ -4,11 +4,29 @@ namespace Open.Sentry.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
-        private int credits = -1;
-        public int Credits
+        private decimal credits = -1;
+        public decimal Credits
         {
-            get => credits == -1 ? 100 : credits;
+            get => credits == -1 ? 1000 : credits;
             set => credits = value;
+        }
+
+        //remove true, if added
+        public bool RemoveCredits(decimal amount)
+        {
+            if (amount > credits) return false;
+            Credits -= amount;
+            return true;
+        }
+
+        public void RemoveAllCredits()
+        {
+            Credits = 0;
+        }
+
+        public void AddCredits(decimal amount)
+        {
+            Credits += amount > 0 ? amount : 0;
         }
     }
 }
