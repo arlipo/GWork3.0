30fb1698a46f56119c10df48c6c4577e3c456e47
Artjom Lipodat <arlipo@ttu.ee>
2019-01-19 00:48:22 +0200

    core tests

--- Open/Open/Tests/Core/GoodTypesTests.cs
+++ Open/Open/Tests/Core/GoodTypesTests.cs
@@ -1,10 +1,34 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Open.Aids;
+using Open.Core;
 
 namespace Open.Tests.Core
 {
     [TestClass]
     public class GoodTypesTests
     {
-        
+        [TestMethod]
+        public void CountTest()
+        {
+            Assert.AreEqual(3, GetEnum.Count<GoodTypes>());
+        }
+
+        [TestMethod]
+        public void ChemistryTest()
+        {
+            Assert.AreEqual(0, (int) GoodTypes.Chemistry);
+        }
+
+        [TestMethod]
+        public void AccessoriesTest()
+        {
+            Assert.AreEqual(1, (int) GoodTypes.Accessories);
+        }
+
+        [TestMethod]
+        public void SparePartsTest()
+        {
+            Assert.AreEqual(2, (int) GoodTypes.SpareParts);
+        }
     }
 }
